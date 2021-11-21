7caa96900c57b80164f27c89ab825f0068869885
jspunna <jspunna214@gmail.com>
2021-11-21 12:02:07 -0500

    making home wireframe branch

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <h1>Tix-Fix is a platform that allows you to buy and selling tickets to events</h1>
         </>
     )
 }
