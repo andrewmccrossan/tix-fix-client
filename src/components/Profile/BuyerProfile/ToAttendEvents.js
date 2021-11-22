7188fe5d6df965e27448d10dcfb3650bfa24861b
jspunna <jspunna214@gmail.com>
2021-11-22 17:21:53 -0500

    slight edit to one of the profile cards

--- src/components/Profile/BuyerProfile/ToAttendEvents.js
+++ src/components/Profile/BuyerProfile/ToAttendEvents.js
@@ -5,7 +5,7 @@ const ToAttendEvents = () => {
     return (
         <>
             <div className="card">
-                <h3 className="card-header h4">Most Recent Attended Events</h3>
+                <h3 className="card-header h4">Upcoming Events to attend</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">Event (similar to postsumary list)</li>
                     <li className="list-group-item">Event (similar to postsumary list)</li>
