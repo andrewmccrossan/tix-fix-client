2dd452a8db2bc1f6706bdb4adb1e671f43db34c1
jspunna <jspunna214@gmail.com>
2021-12-09 14:00:25 -0500

    filling in seller wish list on profile

--- src/components/Profile/SellerProfile/SellerWishListItem.js
+++ src/components/Profile/SellerProfile/SellerWishListItem.js
@@ -2,16 +2,16 @@ import React, {useEffect, useState} from "react";
 import {getEventDetails2} from "../../../services/eventsService";
 import {useHistory} from "react-router-dom";
 
-const TicketsToSellItem = ({sellingItem}) => {
+const SellerWishListItem = ({eventID}) => {
 
     const [event, setEvent] = useState({});
-    useEffect(() => getEventDetails2(sellingItem.eventID).then(results => {setEvent(results)}), [])
+    useEffect(() => getEventDetails2(eventID).then(results => {setEvent(results)}), [])
 
     const history = useHistory();
     const detailsClickHandler = (resultID) => {
         history.push(`/details/${resultID}`);
     }
-    
+
     return (
         <>
             <li className="list-group-item">
@@ -20,12 +20,6 @@ const TicketsToSellItem = ({sellingItem}) => {
                         <div className="col-9">
                             <div className="row fw-bold">
                                 {event.short_title}
-                            </div>
-                            <div className="row">
-                                Tickets Quantity: {sellingItem.qty}
-                            </div>
-                            <div className="row">
-                                Price per ticket: ${sellingItem.price}
                             </div>
                         </div>
                         <div className="col-3">
@@ -51,4 +45,4 @@ const TicketsToSellItem = ({sellingItem}) => {
         </>
     )
 }
-export default TicketsToSellItem;
+export default SellerWishListItem;
