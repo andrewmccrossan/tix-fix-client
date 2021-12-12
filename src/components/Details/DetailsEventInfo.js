75fb7767cd3847eaef8c326ee365d44bea6c6c95
jspunna <jspunna214@gmail.com>
2021-12-12 18:50:37 -0500

    responsive/styling for login/register

--- src/components/Details/DetailsEventInfo.js
+++ src/components/Details/DetailsEventInfo.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
 import {useHistory, useParams} from "react-router-dom";
 import {getEventDetails2} from "../../services/eventsService";
 import {convertMilitaryTime, formatDate} from "../../Utils/utils";
