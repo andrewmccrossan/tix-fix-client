8127b4e57940cae78f00f682b380fb1d7034d263
adam-foley <adam.f.foley1@gmail.com>
2021-12-11 19:01:08 -0500

    Adding logic to handle deleting selling tickets

--- src/components/Profile/BuyerProfile/UpcomingTicketPurchase.js
+++ src/components/Profile/BuyerProfile/UpcomingTicketPurchase.js
@@ -21,7 +21,7 @@ const UpcomingTicketPurchase = ({ currentUser, eventBought, purchaseDetails}) =>
             <li className="list-group-item">
                 <div className="container">
                     <div className="row">
-                        <div className="col-9">
+                        <div className="col-9 align-self-center">
                             <div className="row fw-bold">
                                 {event.short_title}
                             </div>
@@ -32,21 +32,11 @@ const UpcomingTicketPurchase = ({ currentUser, eventBought, purchaseDetails}) =>
                                 Price per ticket: ${purchaseDetails.price}
                             </div>
                         </div>
-                        <div className="col-3">
-                            {/*<div className="d-grid">
-                                <button type="button"
-                                        className="btn btn-info btn-sm float-end mt-3">
-                                    Event Details
-                                </button>
-                                <button type="button"
-                                        className="btn btn-danger btn-sm float-end mt-3">
-                                    Stop selling (doesnt nothing yet)
-                                </button>
-                            </div>*/}
+                        <div className="col-3 align-self-center">
                             <button type="button"
-                                    className="btn btn-info btn-sm float-end mt-3"
+                                    className="btn btn-info btn-sm float-end"
                                     onClick={()=> detailsClickHandler(event.id)}>
-                                Event Details
+                                More Details
                             </button>
                         </div>
                     </div>
--- src/components/Profile/EventsInArea.js
+++ src/components/Profile/EventsInArea.js
@@ -33,7 +33,7 @@ const EventsInArea = ({currentUser}) => {
                                 <li className="list-group-item float-start">
                                     <div className="container">
                                         <div className="row">
-                                            <div className="col">
+                                            <div className="col align-self-center">
                                                 <div className="row fw-bold">
                                                     {event.short_title}
                                                 </div>
@@ -46,10 +46,9 @@ const EventsInArea = ({currentUser}) => {
                                                     {event.venue.city.concat(", ", event.venue.state)}
                                                 </div>
                                             </div>
-
-                                            <div className="col">
+                                            <div className="col align-self-center ">
                                                 <button type="button"
-                                                        className="btn btn-info btn-sm float-end mt-3"
+                                                        className="btn btn-info btn-sm float-end"
                                                         onClick={()=> detailsClickHandler(event.id)}>
                                                     More Details
                                                 </button>
