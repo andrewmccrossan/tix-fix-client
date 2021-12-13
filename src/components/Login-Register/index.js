b078ac1b18104126defbf22a88d3f6388986e067
jspunna <jspunna214@gmail.com>
2021-12-12 19:11:13 -0500

    responsive for homepage, footer to login/register

--- src/components/Login-Register/index.js
+++ src/components/Login-Register/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./Login";
 import Register from "./Register"
 import TopNavBar from "../TopNavBar";
+import Footer from "../Home/Footer";
 
 const LoginRegister = () => {
     return (
@@ -16,13 +17,9 @@ const LoginRegister = () => {
                         <Register/>
                     </div>
                 </div>
+                <hr/>
+                <Footer/>
             </div>
-            <div className="d-block d-sm-none fa-2x">XS</div>
-            <div className="d-none d-sm-block d-md-none fa-2x">S</div>
-            <div className="d-none d-md-block d-lg-none fa-2x">M</div>
-            <div className="d-none d-lg-block d-xl-none fa-2x">L</div>
-            <div className="d-none d-xl-block d-xxl-none fa-2x">XL</div>
-            <div className="d-none d-xxl-block fa-2x">XXL</div>
         </>
     )
 }
