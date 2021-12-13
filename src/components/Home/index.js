bd216c9c2855e70ac627f6fd130adbb190faa81c
jspunna <jspunna214@gmail.com>
2021-12-12 19:43:57 -0500

    responsive search results

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -41,9 +41,7 @@ const Home = () => {
     }
 
     return (
-
         <>
-
             <TopNavBar page={"home"}/>
             <div className="container mt-5">
                 <Description/>
@@ -53,12 +51,6 @@ const Home = () => {
                 <hr/>
                 <Footer/>
             </div>
-            {/*<div className="d-block d-sm-none fa-2x">XS</div>
-            <div className="d-none d-sm-block d-md-none fa-2x">S</div>
-            <div className="d-none d-md-block d-lg-none fa-2x">M</div>
-            <div className="d-none d-lg-block d-xl-none fa-2x">L</div>
-            <div className="d-none d-xl-block d-xxl-none fa-2x">XL</div>
-            <div className="d-none d-xxl-block fa-2x">XXL</div>*/}
         </>
     )
 }
--- src/components/Search/SearchResultsItem.js
+++ src/components/Search/SearchResultsItem.js
@@ -15,15 +15,15 @@ const SearchResultsItem = ({result}) => {
                 <div className="col-lg-1"/>
                 <div className="col-12 col-lg-10">
                     <div className="row border">
-                        <div className="col-2 ">
+                        <div className="d-none d-lg-block col-lg-2 ">
                             <div className="d-flex justify-content-center fw-bold">{formatDate(result.datetime_local)}</div>
                             <div className="d-flex justify-content-center">{convertMilitaryTime(result.datetime_local)}</div>
                         </div>
-                        <div className="col-8">
+                        <div className="col-9 col-lg-8">
                             <div className="d-flex justify-content-center fw-bold">{result.title}</div>
                             <div className="d-flex justify-content-center">{result.venue.name} - {result.venue.display_location}</div>
                         </div>
-                        <div className="col-2 d-flex justify-content-center align-items-center">
+                        <div className="col-3 col-lg-2 d-flex justify-content-center align-items-center">
                             <button type="button"
                                     className="btn btn-primary mt-1 mb-1"
                                     onClick={()=> detailsClickHandler(result.id)}>
