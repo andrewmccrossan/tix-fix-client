e18fe66ee19b78c6c5fc477ee2d400f0598fd969
Adam Foley <80927105+adam-foley@users.noreply.github.com>
2021-12-14 20:02:20 -0500

    Update Footer.js

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <>
             <ul className="tx-home-footer-links blue">
-                <li className="text-info me-2"><Link to="/home" >Home</Link></li>
-                <li className="text-info me-2"><Link to="/profile">Profile</Link></li>
-                <li className="text-info me-2"><Link to="/search">Search</Link></li>
-                <li className="text-info me-2"><Link to="/privacy">Privacy</Link></li>
+                <li className="me-2"><Link to="/home" >Home</Link></li>
+                <li className="me-2"><Link to="/profile">Profile</Link></li>
+                <li className="me-2"><Link to="/search">Search</Link></li>
+                <li className="me-2"><Link to="/privacy">Privacy</Link></li>
             </ul>
         </>
     )
